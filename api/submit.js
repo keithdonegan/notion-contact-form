0d8cd3b422815862f3186fa24b7f7a87a0524b4f
Keith Donegan <keith.donegan@gmail.com>
2025-08-11 18:35:09 +0100

    working

--- api/submit.js
+++ api/submit.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
       properties: {
         Name: {
           title: [
-            {
+            {``
               text: { content: name },
             },
           ],
